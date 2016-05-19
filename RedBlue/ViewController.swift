0db9b2f67036a091f8c8f8fa328564b9a7239a51
ABnyc <arpita.nyc@gmail.com>
2016-05-19 14:51:57 -0400

    Red Fish Blue Fish
    Button to show and hide fish

--- RedBlue/ViewController.swift
+++ RedBlue/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redFish: UIImageView!
+    @IBOutlet weak var blueFish: UIImageView!
+    
+    @IBOutlet weak var redButtonLabel: UIButton!
+    @IBOutlet weak var blueButtonLabel: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func redButton(sender: AnyObject) {
+        
+        if redFish.hidden == false {
+            redFish.hidden = true
+            redButtonLabel.setTitle("Show Red Fish", forState: UIControlState.Normal)
+        } else {
+            redFish.hidden = false
+            redButtonLabel.setTitle("Hide Red Fish", forState: UIControlState.Normal)
+        }
+        
+    }
 
+    @IBAction func blueButton(sender: AnyObject) {
+        
+        if blueFish.hidden == false {
+            blueFish.hidden = true
+            blueButtonLabel.setTitle("Show Blue Fish", forState: UIControlState.Normal)
+        } else {
+            blueFish.hidden = false
+            blueButtonLabel.setTitle("Hide Red Fish", forState: UIControlState.Normal)
+        }
+    }
 }
 
